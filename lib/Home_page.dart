5d1f17fedf7cbf6c60fe2b5792ac82351c86cfc3
dastar2 <davighiggino3@gmail.com>
2020-12-06 20:12:43 -0300

    Construindo  o AppBar da página (incompleto)

--- lib/Home_page.dart
+++ lib/Home_page.dart
@@ -11,17 +11,31 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-  void _incrementCounter() {
-    setState(() {
-     
-      _counter++;
-    });
-  }
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        centerTitle: true,
+        title: Image.asset(
+          'assets/logo.png',
+          fit: BoxFit.contain,
+          height: 70,
+          color: Colors.blue[300],
+        ),
+        
+        leading: IconButton(
+            icon: Icon(Icons.menu, color: Colors.blue[300]), onPressed: null),
+
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.settings, color: Colors.blue[300]),
+              onPressed: null)
+        ],
+        
 
 
 
 
-
+      ),
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
