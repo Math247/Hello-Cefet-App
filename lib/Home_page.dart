67fd4a0982b1c13baf129d1e38f629421cc5b76f
dastar2 <davighiggino3@gmail.com>
2020-12-06 20:11:23 -0300

    Iniciando a construção da home page

--- lib/Home_page.dart
+++ lib/Home_page.dart
@@ -2,16 +2,15 @@ import 'package:flutter/material.dart';
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-
-  final String title;
-
+class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
   void _incrementCounter() {
     setState(() {
      
@@ -19,18 +18,10 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-   
-    return Scaffold(
-      appBar: AppBar(
-        
-        title: Text(widget.title),
-      ),
-      body: Center(
-       
-        child: Column(
-         
+
+
+
+
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
