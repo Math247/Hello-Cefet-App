9aefd16cf772169d8605264351f109e1b0ac0210
dastar2 <davighiggino3@gmail.com>
2020-12-08 06:05:24 -0300

    Implementado o drawer na appbar

--- lib/Home_page.dart
+++ lib/Home_page.dart
@@ -8,50 +8,49 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return MaterialApp(
+        home: Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.white,
+        backgroundColor: Colors.blue[400],
         elevation: 0,
         centerTitle: true,
         title: Image.asset(
           'assets/logo.png',
           fit: BoxFit.contain,
           height: 70,
-          color: Colors.blue[300],
+          color: Colors.white,
         ),
-        leading: IconButton(
-            icon: Icon(Icons.menu, color: Colors.blue[300]), onPressed: null),
         actions: <Widget>[
           IconButton(
-              icon: Icon(Icons.settings, color: Colors.blue[300]),
-              onPressed: null)
+              icon: Icon(Icons.settings, color: Colors.white), onPressed: null)
         ],
       ),
-      body: Stack(
-        children: [
-          Container(
-            color: Colors.blue[300],
-            child: Row(
-              textDirection: TextDirection.ltr,
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Card(
-                      elevation: 5,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: ListTile(),
-                    )
-                  ],
-                ),
-              ],
-            ),
-          )
-        ],
+      body: Container(
+        color: Colors.amber,
       ),
-    );
+      drawer: Drawer(
+          child: ListView(
+        children: <Widget>[
+          ListTile(
+              leading: Icon(Icons.group),
+              title: Text("Membros"),
+              subtitle: Text("Integrantes da equipe..."),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: () {}),
+          ListTile(
+              leading: Icon(Icons.menu_book),
+              title: Text("Grade Horária"),
+              subtitle: Text("Disciplinas..."),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: () {}),
+          ListTile(
+              leading: Icon(Icons.construction_outlined),
+              title: Text("Satisfação"),
+              subtitle: Text("In constrution..."),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: () {})
+        ],
+      )),
+    ));
   }
 }
